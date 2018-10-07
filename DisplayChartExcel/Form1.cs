84c235d438403ec4889f48e1e74818758cd4d04b
Woraprad <31096955+Woraprad@users.noreply.github.com>
2018-10-08 00:39:13 +0700

    complete

--- DisplayChartExcel/Form1.cs
+++ DisplayChartExcel/Form1.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using ExcelDataReader;
 using System.Diagnostics;
+using ZedGraph;
+
 
 namespace DisplayChartExcel
 {
@@ -13,6 +20,7 @@ namespace DisplayChartExcel
         public Form1()
         {
             InitializeComponent();
+            CreateChart(zedGraphControl1);
         }
 
         private static IList<string> GetTablenames(DataTableCollection tables)
@@ -27,7 +35,7 @@ namespace DisplayChartExcel
         }
 
         private DataSet ds;
-        
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -75,6 +83,10 @@ namespace DisplayChartExcel
                 if (tablenames.Count > 0)
                     sheetCombo.SelectedIndex = 0;
 
+
+
+
+                UpdateZedGraph(zedGraphControl1);
                 // dataGridView1.DataSource = ds;
                 // dataGridView1.DataMember
             }
@@ -102,5 +114,56 @@ namespace DisplayChartExcel
                 textBox1.Text = openFileDialog1.FileName;
             }
         }
+
+        public void CreateChart(ZedGraphControl zgc)
+        {
+            GraphPane myPane = zgc.GraphPane;
+            // Set the titles and axis labels
+            myPane.Title.Text = "Demonstration of Dual Y Graph";
+            myPane.XAxis.Title.Text = "Number";
+            myPane.YAxis.Title.Text = "Value";
+            // Make up some data points 
+            PointPairList list = new PointPairList();
+            // Generate a red curve with diamond symbols, and "Alpha" in the legend
+            LineItem myCurve = myPane.AddCurve("Data",
+               list, Color.Red, SymbolType.Diamond);
+            // Calculate the Axis Scale Ranges
+            zgc.AxisChange();
+        }
+        private void UpdateZedGraph(ZedGraphControl zedGraphControl1)
+        {
+            // Make sure that the curvelist has at least one curve
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return;
+
+            // Get the first CurveItem in the graph
+            LineItem curve = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (curve == null)
+                return;
+
+            // Get the PointPairList
+            IPointListEdit list = curve.Points as IPointListEdit;
+            // If this is null, it means the reference at curve.Points does not
+            // support IPointListEdit, so we won't be able to modify it
+            if (list == null)
+                return;
+
+            list.Clear();
+            int maxrows = ds.Tables[0].Rows.Count;
+            Console.WriteLine("ds.Tables[0].Rows.Count = {0}", maxrows);
+
+            for (int i = 0; i < maxrows; i++)
+            {
+                var a = ds.Tables[0].Rows[i];
+                double x = (double)a.ItemArray.GetValue(0);
+                double y = (double)a.ItemArray.GetValue(0);
+                list.Add(x, y);
+            }
+            // Make sure the Y axis is rescaled to accommodate actual data
+            zedGraphControl1.AxisChange();
+            // Force a redraw
+            zedGraphControl1.Invalidate();
+        }
     }
 }
+
